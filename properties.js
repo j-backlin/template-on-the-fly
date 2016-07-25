17366f0b84fe591075be83d64c537b505d874b0e
Johan <baecklin@gmail.com>
2016-07-25 14:16:45 +0200

    Add files via upload

---
+++ properties.js
@@ -0,0 +1,41 @@
+/**
+ * @owner Johan Bäcklin (jbi)
+ */ 
+define(["qlik"], function(qlik) {
+	var definition = {
+        type: "items",
+        component: "accordion",
+        items: {			
+			apps: {
+					ref: "apps",
+					label: "Apps",
+					type: "string",
+					component: "dropdown",
+					options: function() {
+						return getApps().then((apps) => {
+							return apps;							
+						})	
+						function getApps(){return new Promise(function(resolve, reject) {
+							qlik.getAppList(function(jbiList){
+								var apps = [{
+									value: "",
+									label: "No app chosen"
+								}].concat(jbiList.map(function(jbiInfo) {
+									return {
+										value: jbiInfo.qDocId,
+										label: jbiInfo.qDocName
+									};
+								}))
+								resolve(apps);
+							})					
+						})
+						}
+					}
+				},
+            settings: {
+                uses: "settings"
+            }
+        }
+    };
+    return definition;
+});
